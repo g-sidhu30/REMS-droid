eec0d7d33678e98fef16f71e45de943480108267
g-sidhu30 <gsidh375@mtroyal.ca>
2017-06-08 01:07:15 -0600

    ui
    Changed UI, added more functions

--- app/src/main/java/com/example/rp/holliday/MainActivity.java
+++ app/src/main/java/com/example/rp/holliday/MainActivity.java
@@ -2,23 +2,35 @@ package com.example.rp.holliday;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import custom_font.MyTextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView holliday;
-    MyTextView create;
+    MyTextView createText;
+    TextView loginText;
+    TextView
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signup);
+        setContentView(R.layout.activity_main);
 
-      //  create = (MyTextView)findViewById(R.id.create);
-      //  holliday = (TextView)findViewById(R.id.holliday);
+        createText = (MyTextView)findViewById(R.id.create);
+
 
       //  Typeface custom_fonts = Typeface.createFromAsset(getAssets(), "fonts/ArgonPERSONAL-Regular.otf");
       //  holliday.setTypeface(custom_fonts);
+
+
+    }
+
+    public void onClick(View v){
+        setContentView(R.layout.signup);
+    }
+
+    public void loginClick(View v){
 
     }
 }
